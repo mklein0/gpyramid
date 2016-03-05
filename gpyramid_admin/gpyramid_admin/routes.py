a62f7598886069b2de35bc802e38f80c1dfecfaa
Marcos Klein <mklein0@gmail.com>
2016-03-05 16:11:49 +0000

    Add a redirect to root path to first level of admin WSGI.

--- gpyramid_admin/gpyramid_admin/routes.py
+++ gpyramid_admin/gpyramid_admin/routes.py
@@ -6,7 +6,8 @@ def add_routes(config):
     """
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    config.add_route('home', '/')
+    # config.add_route('home', '/')
+    config.add_route('home.redirect', '/')
 
     # Configuration End-Points
     config.add_route('data.index', 'data/')
--- gpyramid_admin/gpyramid_admin/views/old.py
+++ gpyramid_admin/gpyramid_admin/views/old.py
@@ -1,6 +1,17 @@
+#
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'gpyramid_admin'}
+# @view_config(route_name='home', renderer='../templates/mytemplate.pt')
+# def my_view(request):
+#     return {'project': 'gpyramid_admin'}
+
+
+@view_config(route_name='home.redirect')
+def my_view_redirect(request):
+    """
+
+    :param pyramid.request.Request request: Incoming Web Request
+    """
+    return HTTPFound(location=request.route_path('data.index'))
